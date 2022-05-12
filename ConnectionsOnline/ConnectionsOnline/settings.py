83f28f0549f979aa6bf310d010f2ff443060f30d
Daniel165c <98443241+Daniel165c@users.noreply.github.com>
2022-05-12 19:22:20 -0400

    daniel_add_stff

--- ConnectionsOnline/ConnectionsOnline/settings.py
+++ ConnectionsOnline/ConnectionsOnline/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'connectionsonline.apps.ConnectionsOnlineConfig',
+    'jobpostings.app.JobPostingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
