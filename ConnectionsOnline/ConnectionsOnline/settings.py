cc817f0750e8ee4b31c237113e9ba75360274249
Daniel165c <98443241+Daniel165c@users.noreply.github.com>
2022-05-12 19:15:44 -0400

    add app

--- ConnectionsOnline/ConnectionsOnline/settings.py
+++ ConnectionsOnline/ConnectionsOnline/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'connectionsonline.apps.ConnectionsOnlineConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
