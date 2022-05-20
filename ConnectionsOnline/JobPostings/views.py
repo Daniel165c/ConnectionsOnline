2d14fecbb4bdf8dc1c94612988eb2c6517f243ac
Infinite Rahming <irahming83@gmail.com>
2022-05-20 12:37:05 -0400

    Modify views.py

--- ConnectionsOnline/JobPostings/views.py
+++ ConnectionsOnline/JobPostings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
-
+from .models import Job
+from JobPostings.forms import JobListingForm
 # from paintapp.forms import ColorPickerForm
 
 # Create your views here.
@@ -11,5 +12,16 @@ class ConnectionsOnlineView(View):
 
 class PostJobsViews(View):
     def get(self, request):
+        form = JobListingForm()  
         return render(request=request,
-        template_name='postjobs.html')
+        template_name='postjobs.html',context = {'form': form})
+
+    def post(self, request):
+        '''POST the data in the from submitted by the user, creating a new task in the todo list'''
+        form = JobListingForm (request.POST)
+        if form.is_valid():
+          description = form.cleaned_data['JobTitle']
+          Job.objects.create(job_description=description)
+
+        # "redirect" to the postjob homepage
+        return redirect('postjobs')  
