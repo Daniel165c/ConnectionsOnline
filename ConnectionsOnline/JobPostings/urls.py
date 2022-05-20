eba1d181b50f91236f183eac4c9bd651501aa8e6
Infinite Rahming <irahming83@gmail.com>
2022-05-20 12:36:06 -0400

    Modify urls.py

--- ConnectionsOnline/JobPostings/urls.py
+++ ConnectionsOnline/JobPostings/urls.py
@@ -5,6 +5,6 @@ from JobPostings.views import *
 urlpatterns = [
    
     path('', ConnectionsOnlineView.as_view(), name='home'),
-    path('postjobs/', PostJobsViews.as_view(), name='postjobs.html'
+    path('postjobs/', PostJobsViews.as_view(), name='postjobs'
     )
 ]
