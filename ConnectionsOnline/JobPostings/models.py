a80734056c7c150fab8a27b62c0f26fd8fb22ebc
Infinite Rahming <irahming83@gmail.com>
2022-05-20 12:33:01 -0400

    modify models.py

--- ConnectionsOnline/JobPostings/models.py
+++ ConnectionsOnline/JobPostings/models.py
@@ -4,3 +4,4 @@ class Job(models.Model):
     job_title = models.CharField(max_length=30)
     job_description = models.CharField(max_length=30)
     job_requirements = models.CharField(max_length=30)
+    
