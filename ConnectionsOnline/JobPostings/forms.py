2ed524c0eccc686c8738a8f96cc6654a3f5684fb
Infinite Rahming <irahming83@gmail.com>
2022-05-26 21:24:59 -0400

    Update forms

--- ConnectionsOnline/JobPostings/forms.py
+++ ConnectionsOnline/JobPostings/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Job
 
 class JobListingForm(forms.Form):
-    JobTitle = forms.CharField(label='Post Job', max_length=255)
+    JobTitle= forms.CharField(label='Job Title', max_length=255)
+    JobDescription = forms.CharField(widget=forms.Textarea, label='Job Description')
