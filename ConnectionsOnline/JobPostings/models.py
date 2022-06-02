5ab4cee528e8043fef0e62b01b5a530c99e59db3
Djenkins1221 <donelljenkins122199@gmail.com>
2022-06-02 19:25:11 -0400

    updated

--- ConnectionsOnline/JobPostings/models.py
+++ ConnectionsOnline/JobPostings/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
-# Create your models here.
 
-
-class Job (models.model):
+class Job (models.Model):
     job_title = models.CharField(max_length=30)
     job_descriptions = models.CharField(max_length=30)
     job_requirements = models.CharField(max_length=30)
