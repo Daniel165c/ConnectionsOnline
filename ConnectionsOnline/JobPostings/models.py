b5ba78119a1b34f7f291fe79469276062647fcd4
Daniel165c <98443241+Daniel165c@users.noreply.github.com>
2022-06-02 22:44:48 -0400

    new work

--- ConnectionsOnline/JobPostings/models.py
+++ ConnectionsOnline/JobPostings/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-class Job(models.Model):
-    job_title = models.CharField(max_length=30)
-    job_description = models.CharField(max_length=30)
-    job_requirements = models.CharField(max_length=30)
+class Jobs(models.Model):
+    job_title = models.CharField(max_length=30, null=True, blank=True)
+    job_description = models.CharField(max_length=250, null=True, blank=True)
--- ConnectionsOnline/JobPostings/urls.py
+++ ConnectionsOnline/JobPostings/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from JobPostings.views import *
 
+from JobPostings import views
 
 urlpatterns = [
-   
-    path('', ConnectionsOnlineView.as_view(), name='home'),
-    path('postjobs/', PostJobsViews.as_view(), name='postjobs.html'
-    )
+    path('', views.ConnectionsOnlineView.as_view(), name='home'),
+    path('postjobs/create_post', views.addrecord, name='add_job'),
+    path('postjobs/', views.index, name='index'),
+    path('postjobs/delete/<int:id>', views.delete, name='delete_job'),
+    path('postjobs/update', views.update, name='confirm_update'),
+    path('postjobs/edit/<int:id>', views.ConnectionsOnlineUpdateView.as_view(), name="update_job")
 ]
